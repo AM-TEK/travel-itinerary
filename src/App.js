cd70ce130b5c0dd42d05e9569b76d75d23e31b39
Alejandro Maldonado <am@AM-WEBDEV.local>
2024-02-06 09:48:42 -0600

    handling submission of form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -23,21 +24,42 @@ function Logo() {
 }
 
 function Form() {
+  const [desc, setDesc] = useState("");
+  const [quantity, setQuantity] = useState(1)
 
   function handleSubmit(e) {
     e.preventDefault();
+    
+    if (!desc) return;
+    
+    const newItem = {desc, quantity, packed: false, id: Date.now()};
+    console.log(newItem)
+
+    setDesc("");
+    setQuantity(1);
   }
 
   return (
-    <div className="add-form" onSubmit={handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select 
+        value={quantity} 
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({length: 20}, (_, i) => i + 1)
         .map(num => <option value={num} key={num}>{num}</option>)}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input 
+        type="text" 
+        placeholder="Item..." 
+        value={desc} 
+        onChange={(e) => {
+          // console.log(e.target)
+          setDesc(e.target.value)
+        }} 
+      />
       <button>Add</button>
-    </div>
+    </form>
   )
 }
 
